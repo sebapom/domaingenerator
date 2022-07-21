799b3233213c227b4971eabc0b70a50cd00e1d6c
sebapom <sebapobletemorales@gmail.com>
2022-07-21 00:41:10 +0000

    subir tarea

--- src/app.js
+++ src/app.js
@@ -6,6 +6,32 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  var pronoun = ["el", "la", "los", "las", "un", "una", "unos", "unas"];
+  var adj = [
+    "maravillosa",
+    "grande",
+    "gran",
+    "espectacular",
+    "tenebrosa",
+    "tenebroso",
+    "maravilloso"
+  ];
+  var noun = [
+    "caminante",
+    "rata",
+    "raton",
+    "tipo",
+    "tipa",
+    "chico",
+    "chica",
+    "mundo"
+  ];
+
+  for (let i = 0; i < pronoun.length; i++) {
+    for (let j = 0; j < adj.length; j++) {
+      for (let x = 0; x < noun.length; x++) {
+        console.log(pronoun[i], adj[j], noun[x], ".cl");
+      }
+    }
+  }
 };
